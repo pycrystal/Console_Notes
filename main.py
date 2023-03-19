003de5d3b1e89861447cb8c38eca83d3a9e545c4
Desart <desart@192.168.100.6>
2023-03-19 19:19:37 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,148 +1,185 @@
 from art import tprint
 import pickle
+from simple_term_menu import TerminalMenu
 
 #Красивая надпись
 tprint('Notes')
 
 #Главное меню
-print('Команды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-user_input = int(input())
+choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+user_input = TerminalMenu(choise, title="Команды:")
+user_input.show()
+
 
 #Что-бы сохранить
 while True:
-    monday = 'input()'
-    tuesday = 'input()'
-    wednesday = 'input()'
-    thursday = 'input()'
-    friday = 'input()'
-    saturday = 'input()'
-    sunday = 'input()'
+    monday = ''
+    tuesday = ''
+    wednesday = ''
+    thursday = ''
+    friday = ''
+    saturday = ''
+    sunday = ''
 
     #Меню для записи заметок
-    if user_input == 2:
-        user_day = int(input('Выберите день недели:\n\t1 - Понедельник\n\t2 - Вторник\n\t3 - Среда\n\t4 - Четверг\n\t5 - Пятница\n\t6 - Суббота\n\t7 - Воскресенье\n'))
+    if user_input.chosen_menu_index == 1:
+        num = ['1 - Понедельник', '2 - Вторник', '3 - Среда', '4 - Четверг', '5 - Пятница', '6 - Суббота', '7 - Воскресенье']
+        menu = TerminalMenu(num, title="Выберите день недели:")
+        menu.show()
+        
         #Понедельник
-        if user_day == 1:
+        if menu.chosen_menu_index == 0:
             monday = input()
             f = open('mon.bin', 'wb')
             pickle.dump(monday, f)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Вторник     
-        elif user_day == 2:
+        elif menu.chosen_menu_index == 1:
             tuesday = input()
             f = open('tue.bin', 'wb')
             pickle.dump(tuesday, f)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Среда
-        elif user_day == 3:
+        elif menu.chosen_menu_index == 2:
             wednesday = input() 
             f = open('wed.bin', 'wb')
             pickle.dump(wednesday, f)
             f.close()  
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())  
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()  
+
         #Четверг      
-        elif user_day == 4:
+        elif menu.chosen_menu_index == 3:
             thursday = input()
             f = open('thu.bin', 'wb')
             pickle.dump(thursday, f)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Пятница
-        elif user_day == 5:
+        elif menu.chosen_menu_index == 4:
             friday = input()
             f = open('fri.bin', 'wb')
             pickle.dump(friday, f)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Суббота
-        elif user_day == 6:
+        elif menu.chosen_menu_index == 5:
             saturday = input()
             f = open('sat.bin', 'wb')
             pickle.dump(saturday, f)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Воскресенье
-        elif user_day == 7:
+        elif menu.chosen_menu_index == 6:
             sunday = input()
             f = open('sun.bin', 'wb')
             pickle.dump(sunday, f)
             f.close()   
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())     
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()   
+
     #Меню для вывода заметок
-    if user_input == 1:
-        user_out = int(input('Выберите день недели:\n\t1 - Понедельник\n\t2 - Вторник\n\t3 - Среда\n\t4 - Четверг\n\t5 - Пятница\n\t6 - Суббота\n\t7 - Воскресенье\n'))
+    if user_input.chosen_menu_index == 0:
+        num = ['1 - Понедельник', '2 - Вторник', '3 - Среда', '4 - Четверг', '5 - Пятница', '6 - Суббота', '7 - Воскресенье']
+        menu = TerminalMenu(num, title="Выберите день недели:")
+        menu.show()
+
         #Понедельник
-        if user_out == 1:
+        if menu.chosen_menu_index == 0:
             tprint('Monday')
             f = open('mon.bin', 'rb')
             monday_output = pickle.load(f)
             print(monday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Вторник
-        elif user_out == 2:
+        elif menu.chosen_menu_index == 1:
             tprint('Tuesday')
             f = open('tue.bin', 'rb')
             tuesday_output = pickle.load(f)
             print(tuesday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Среда
-        elif user_out == 3:
+        elif menu.chosen_menu_index == 2:
             tprint('Wednesday')
             f = open('wed.bin', 'rb')
             wednesday_output = pickle.load(f)
             print(wednesday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input()) 
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Четверг
-        elif user_out == 4:
+        elif menu.chosen_menu_index == 3:
             tprint('Thursday')
             f = open('thu.bin', 'rb')
             thursday_output = pickle.load(f)
             print(thursday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Пятница  
-        elif user_out == 5:
+        elif menu.chosen_menu_index == 4:
             tprint('Friday')
             f = open('fri.bin', 'rb')
             friday_output = pickle.load(f)
             print(friday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Суббота
-        elif user_out == 6:
+        elif menu.chosen_menu_index == 5:
             tprint('Saturday')
             f = open('sat.bin', 'rb')
             saturday_output = pickle.load(f)
             print(saturday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input())
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show()
+
         #Воскресенье
-        elif user_out == 7:
+        elif menu.chosen_menu_index == 6:
             tprint('Sunday')
             f = open('sun.bin', 'rb')
             sunday_output = pickle.load(f)
             print(sunday_output)
             f.close()
-            print('\nКоманды:\n1 - Посмотреть заметки\n2 - Добавить заметку\n3 - Выход')
-            user_input = int(input()) 
+            choise = ['1 - Посмотреть заметки', '2 - Добавить заметку', '3 - Выход']
+            user_input = TerminalMenu(choise, title="Команды:")
+            user_input.show() 
+
      #Выход       
-    if user_input == 3:
-        break        
+    if user_input.chosen_menu_index == 2:
+        break 
